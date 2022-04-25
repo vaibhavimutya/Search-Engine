efdb3976da2dcae078908bcc0bb5a453b8aa0e7c
Vaibhavi Mutya <65438944+vaibhavimutya@users.noreply.github.com>
2022-04-24 19:59:53 -0700

    Add files via upload

---
+++ InstallDependencies.sh
@@ -0,0 +1,12 @@
+sudo apt-get install python-pip
+
+sudo apt-get install python-pip
+
+virtualenv crawlEnv
+
+crawlEnv/bin/pip beautifulsoup4
+
+crawlEnv/bin/pip install urllib3
+crawlEnv/bin/pip install pathlib
+
+mkdir /opt/home/cs242-w22/dataset
